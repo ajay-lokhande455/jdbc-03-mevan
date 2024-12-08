5b71e0f6724b66783a96a7e06b62dfc9e0c035ec
Ajay Lokhande <lokhandeajay455@gmail.com>
2024-12-08 14:14:45 +0530

    java projects

---
+++ jdbc_maven/src/main/java/com/firstproject/App.java
@@ -0,0 +1,22 @@
+package com.firstproject;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    @AllArgsConstructor;
+    @NoArgsConstructor
+    @Builder
+    @Getter
+    public static void main( String[] args )
+    {
+
+    }
+}
